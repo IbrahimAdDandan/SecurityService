a32dde457d049e3aede1cb02b6288f38142e31e5
Ibrahim AdDandan <ibrahim.ad.dandan@gmail.com>
2020-07-04 12:25:07 +0300

    artifacts .jar

--- src/main/java/com/myframework/sbase/controller/Users.java
+++ src/main/java/com/myframework/sbase/controller/Users.java
@@ -22,50 +22,50 @@ public class Users {
         this.authService = authService;
     }
 
-//    @GetMapping("")
-//    public ResponseEntity<?> getAll(Authentication authentication) {
-//        if (!AuthorityHelper.hasAuthority(authentication.getAuthorities(), this.getClass().getSimpleName(), OperationType.GET))
-//            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
-//        return new ResponseEntity<>(authService.getUsers(), HttpStatus.OK);
-//    }
+    @GetMapping("")
+    public ResponseEntity<?> getAll(Authentication authentication) {
+        if (!AuthorityHelper.hasAuthority(authentication.getAuthorities(), this.getClass().getSimpleName(), OperationType.GET))
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        return new ResponseEntity<>(authService.getUsers(), HttpStatus.OK);
+    }
 
-//    @GetMapping("/{id}")
-//    public ResponseEntity<?> getUserById(@PathVariable("id") Long userId, Authentication authentication) {
-//        if (!AuthorityHelper.hasAuthority(authentication.getAuthorities(), this.getClass().getSimpleName(), OperationType.GET))
-//            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
-//        return new ResponseEntity<>(authService.getUserById(userId), HttpStatus.OK);
-//    }
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getUserById(@PathVariable("id") Long userId, Authentication authentication) {
+        if (!AuthorityHelper.hasAuthority(authentication.getAuthorities(), this.getClass().getSimpleName(), OperationType.GET))
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        return new ResponseEntity<>(authService.getUserById(userId), HttpStatus.OK);
+    }
 
-//    @PutMapping("")
-//    public ResponseEntity<?> updateUser(@RequestBody User user, Authentication authentication) {
-//        if (!AuthorityHelper.hasAuthority(authentication.getAuthorities(), this.getClass().getSimpleName(), OperationType.UPDATE))
-//            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
-//        return new ResponseEntity<>(authService.add(user), HttpStatus.OK);
-//    }
+    @PutMapping("")
+    public ResponseEntity<?> updateUser(@RequestBody User user, Authentication authentication) {
+        if (!AuthorityHelper.hasAuthority(authentication.getAuthorities(), this.getClass().getSimpleName(), OperationType.UPDATE))
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        return new ResponseEntity<>(authService.add(user), HttpStatus.OK);
+    }
 
-//    @PutMapping("/change-password")
-//    public ResponseEntity<?> updatePassword(@RequestBody String password, Authentication authentication) {
-//        User user = (User)authentication.getPrincipal();
-//        Long userId = user.getId();
-//        User user1 = authService.getUserById(userId).get();
-//        user1.setPassword("{noop}"+password);
-//        authService.add(user1);
-//        return new ResponseEntity<>(HttpStatus.OK);
-//    }
-//
-//    @DeleteMapping("/{id}")
-//    public ResponseEntity<?> deleteUser(@PathVariable("id") Long userId, Authentication authentication) {
-//        if (!AuthorityHelper.hasAuthority(authentication.getAuthorities(), this.getClass().getSimpleName(), OperationType.DELETE))
-//            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
-//        authService.remove(userId);
-//        return new ResponseEntity<>(HttpStatus.OK);
-//    }
-//
-//    @PostMapping("")
-//    public ResponseEntity<?> addUser(@RequestBody User user, Authentication authentication) {
-//        if (!AuthorityHelper.hasAuthority(authentication.getAuthorities(), this.getClass().getSimpleName(), OperationType.ADD))
-//            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
-//        return new ResponseEntity<>(authService.add(user), HttpStatus.OK);
-//    }
+    @PutMapping("/change-password")
+    public ResponseEntity<?> updatePassword(@RequestBody String password, Authentication authentication) {
+        User user = (User)authentication.getPrincipal();
+        Long userId = user.getId();
+        User user1 = authService.getUserById(userId).get();
+        user1.setPassword("{noop}"+password);
+        authService.add(user1);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteUser(@PathVariable("id") Long userId, Authentication authentication) {
+        if (!AuthorityHelper.hasAuthority(authentication.getAuthorities(), this.getClass().getSimpleName(), OperationType.DELETE))
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        authService.remove(userId);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PostMapping("")
+    public ResponseEntity<?> addUser(@RequestBody User user, Authentication authentication) {
+        if (!AuthorityHelper.hasAuthority(authentication.getAuthorities(), this.getClass().getSimpleName(), OperationType.ADD))
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        return new ResponseEntity<>(authService.add(user), HttpStatus.OK);
+    }
 
 }
